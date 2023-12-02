76ecdadc319cc2a033daf05814efdf3ddd2a36e6
billow <billow.fun@gmail.com>
2023-12-01 20:49:25 -0800

    Fix

--- capstone-sys/src/bin/cs_test_tricore.rs
+++ capstone-sys/src/bin/cs_test_tricore.rs
@@ -2,12 +2,14 @@ use capstone_sys::cs_arch::CS_ARCH_TRICORE;
 use capstone_sys::cs_opt_type::CS_OPT_DETAIL;
 use capstone_sys::cs_opt_value::CS_OPT_ON;
 use capstone_sys::{cs_close, cs_disasm, cs_free, cs_insn, cs_mode, cs_open, cs_option, csh};
+use std::ffi::CStr;
 // use rayon::current_thread_index;
 use rayon::prelude::*;
 // use std::ffi::CStr;
+use rayon::current_thread_index;
 use std::mem::MaybeUninit;
 
-fn f(inp: u32) {
+fn f(addr: u64, inp: u32) {
     let mut handle: csh = 0;
     unsafe {
         let err = cs_open(CS_ARCH_TRICORE, cs_mode::CS_MODE_TRICORE_162, &mut handle);
@@ -18,7 +20,7 @@ fn f(inp: u32) {
 
         let mut pinsns = MaybeUninit::<*mut cs_insn>::zeroed();
         let x = inp.to_le_bytes();
-        let count = cs_disasm(handle, x.as_ptr(), x.len(), 0, 1, pinsns.as_mut_ptr());
+        let count = cs_disasm(handle, x.as_ptr(), x.len(), addr, 0, pinsns.as_mut_ptr());
 
         // let insns = std::slice::from_raw_parts(*pinsns.as_ptr(), count);
         // for i in insns {
@@ -36,5 +38,8 @@ fn f(inp: u32) {
 }
 
 fn main() {
-    (0..u32::MAX).into_par_iter().for_each(|x| f(x))
+    (0..u32::MAX).into_par_iter().for_each(|x| f(0, x));
+    (0..u32::MAX)
+        .into_par_iter()
+        .for_each(|x| f(0xffff_ffff, x));
 }
